35145ca06dee5136194e62db0090cbcfa0f4c7b1
Chiller <50303399+Chiller2019@users.noreply.github.com>
2023-06-17 05:03:34 +0200

    #1 APC-UPS
    Insert Template

--- src/components/services/APC_UPS.vue
+++ src/components/services/APC_UPS.vue
@@ -1,1 +1,21 @@
+<template>
+  <Generic :item="item">
+    <template #indicator>
+      <!-- top right area, empty by default -->
+    </template>
+  </Generic>
+</template>
 
+<script>
+import Generic from "./Generic.vue";
+
+export default {
+  name: "APC_UPS",
+  props: {
+    item: Object,
+  },
+  components: {
+    Generic,
+  }
+};
+</script>
